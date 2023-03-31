34c776b6bcb1f2a61c2916790e68e434e5cca926
MH-30 <71397693+MH-30@users.noreply.github.com>
2023-03-31 16:39:43 -0400

    Update CustomerRepositoryTest.java

--- customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTest.java
+++ customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTest.java
@@ -12,13 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=CustomerDataServiceApplication.class)
 public class CustomerRepositoryTest {
-    @MockBean
+   // @MockBean
     @Autowired
     CustomerRepository customerRepository;
 
@@ -28,8 +29,8 @@ public class CustomerRepositoryTest {
     }
 
     @Test
-    @Transactional
-    public void testPutCustomer() {
+    //@Transactional
+    public void testFindByState() {
         Customer customer = new Customer();
         customer.setCustomerId(1);
         customer.setFirstName("Roger");
@@ -39,7 +40,7 @@ public class CustomerRepositoryTest {
         customer.setPhone("123-456-7890");
         customer.setAddress1("12345 North Street");
         customer.setAddress2("12345 South Street");
-        customer.setState("Ohio");
+        customer.setState("OH");
         customer.setCity("Columbus");
         customer.setPostalCode("12345");
         customer.setCountry("United States of America");
@@ -55,7 +56,7 @@ public class CustomerRepositoryTest {
         customer2.setPhone("123-456-7890");
         customer2.setAddress1("12345 North Street");
         customer2.setAddress2("12345 South Street");
-        customer2.setState("Ohio");
+        customer2.setState("OH");
         customer2.setCity("Columbus");
         customer2.setPostalCode("12345");
         customer2.setCountry("United States of America");
@@ -71,7 +72,7 @@ public class CustomerRepositoryTest {
         customer3.setPhone("123-456-7890");
         customer3.setAddress1("12345 North Street");
         customer3.setAddress2("12345 South Street");
-        customer3.setState("Ohio");
+        customer3.setState("OH");
         customer3.setCity("Columbus");
         customer3.setPostalCode("12345");
         customer3.setCountry("United States of America");
@@ -87,17 +88,116 @@ public class CustomerRepositoryTest {
         customer4.setPhone("123-456-7890");
         customer4.setAddress1("12345 North Street");
         customer4.setAddress2("12345 South Street");
-        customer4.setState("Ohio");
+        customer4.setState("OH");
         customer4.setCity("Columbus");
         customer4.setPostalCode("12345");
         customer4.setCountry("United States of America");
 
         customerRepository.save(customer4);
 
-        List<Customer> listOfCustomers = customerRepository.findByState("Ohio");
+        List<Customer> listOfCustomers = customerRepository.findByState("OH");
         assertEquals(4, listOfCustomers.size());
+        //System.out.println(customerRepository.findAll());
 
     }
 
-}
+    @Test
+    public void testGetCustomerById() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1);
+        customer.setFirstName("Roger");
+        customer.setLastName("Federer");
+        customer.setEmail("theSwissMaestro@gmail.com");
+        customer.setCompany("Wimbledon");
+        customer.setPhone("123-456-7890");
+        customer.setAddress1("12345 North Street");
+        customer.setAddress2("12345 South Street");
+        customer.setState("OH");
+        customer.setCity("Columbus");
+        customer.setPostalCode("12345");
+        customer.setCountry("United States of America");
 
+        customerRepository.save(customer);
+
+        Optional<Customer> findCustomer = customerRepository.findById(1);
+
+        assertEquals(customer, findCustomer.get());
+
+    }
+
+    @Test
+    public void testPutCustomerId() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1);
+        customer.setFirstName("Roger");
+        customer.setLastName("Federer");
+        customer.setEmail("theSwissMaestro@gmail.com");
+        customer.setCompany("Wimbledon");
+        customer.setPhone("123-456-7890");
+        customer.setAddress1("12345 North Street");
+        customer.setAddress2("12345 South Street");
+        customer.setState("OH");
+        customer.setCity("Columbus");
+        customer.setPostalCode("12345");
+        customer.setCountry("United States of America");
+
+        customerRepository.save(customer);
+
+        customer.setCountry("Orlando");
+
+        customerRepository.save(customer);
+
+        Optional<Customer> findCustomer = customerRepository.findById(1);
+
+        assertEquals(customer, findCustomer.get());
+
+    }
+
+    @Test
+    public void testCreateCustomer() {
+        Customer customer = new Customer();
+        customer.setFirstName("Roger");
+        customer.setLastName("Federer");
+        customer.setEmail("theSwissMaestro@gmail.com");
+        customer.setCompany("Wimbledon");
+        customer.setPhone("123-456-7890");
+        customer.setAddress1("12345 North Street");
+        customer.setAddress2("12345 South Street");
+        customer.setState("OH");
+        customer.setCity("Columbus");
+        customer.setPostalCode("12345");
+        customer.setCountry("United States of America");
+
+        customer = customerRepository.save(customer);
+
+        assertEquals(customer, customerRepository.findById(customer.getCustomerId()).get());
+
+    }
+
+    @Test
+    public void testDeleteCustomer() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1);
+        customer.setFirstName("Roger");
+        customer.setLastName("Federer");
+        customer.setEmail("theSwissMaestro@gmail.com");
+        customer.setCompany("Wimbledon");
+        customer.setPhone("123-456-7890");
+        customer.setAddress1("12345 North Street");
+        customer.setAddress2("12345 South Street");
+        customer.setState("OH");
+        customer.setCity("Columbus");
+        customer.setPostalCode("12345");
+        customer.setCountry("United States of America");
+
+        customerRepository.save(customer);
+
+        customerRepository.deleteById(customer.getCustomerId());
+        Optional<Customer> deletedCustomer = customerRepository.findById(customer.getCustomerId());
+        assertFalse(deletedCustomer.isPresent());
+    }
+
+
+
+
+}
